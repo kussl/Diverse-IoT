edbbd2378fdd464f8f997b763da21632017a32ee
Hussain Almohri <almohri@cs.ku.edu.kw>
2021-01-05 15:56:16 +0300

    new simulation added

--- driver.py
+++ driver.py
@@ -1,7 +1,109 @@
 from simulator import Simulator
-import os 
+import os, statistics as stats 
+import numpy as np
+
 
 S = Simulator()
-beta = .5
-sigma = .5
-S.simulate_basic(beta,sigma)
+
+# def exp_stepwise():
+# 	OX = [] 
+# 	labels = [] 
+# 	O,i, IS = S.simulate_stepwise(.9,.1)
+# 	OX.append(IS)
+# 	labels.append('β=0.9 ω=0.1')
+
+# 	O,i, IS = S.simulate_stepwise(.5,.5)
+# 	OX.append(IS)
+# 	labels.append('β=0.5 ω=0.5')
+
+# 	O,i, IS = S.simulate_stepwise(.1,.9)
+# 	OX.append(IS)
+# 	labels.append('β=0.1 ω=0.9')
+	
+# 	return OX,labels,i
+
+def exp_stepwise():
+	OX = [] 
+	labels = [] 
+	P = .9
+
+	N = 10
+
+	def one_iteration(P,OX,labels,N,a,b):
+		ISA = [] 
+		i = 0 
+		for j in range(N):
+			O,i, IS = S.simulate_stepwise(a,b)
+			ISA.append(np.array(IS))
+
+		mean = np.mean(ISA, axis=0).tolist()
+		OX.append(mean)
+		labels.append('β={} ω={}'.format(a,b))
+		return i 
+
+	one_iteration(P,OX,labels,N,.9,.1)
+	#P = round(P*.5,2)
+	i = one_iteration(P,OX,labels,N,.5,.5)
+	#P = round(P*.8,2)
+	i = one_iteration(P,OX,labels,N,.1,.9)
+
+	return OX,labels,i
+
+def exp_inaccurate():
+	OX = [] 
+	labels = [] 
+	P = .9
+
+	N = 10
+
+	def one_iteration(P,OX,labels,N):
+		ISA = [] 
+		i = 0 
+		for j in range(N):
+			O,i, IS = S.simulate_inaccurate(.9,.1,P)
+			ISA.append(np.array(IS))
+
+		mean = np.mean(ISA, axis=0).tolist()
+		OX.append(mean)
+		labels.append('P={}'.format(P))
+		return i 
+
+	one_iteration(P,OX,labels,N)
+	#P = round(P*.5,2)
+	i = one_iteration(.7,OX,labels,N)
+	#P = round(P*.8,2)
+	i = one_iteration(.5,OX,labels,N)
+
+	return OX,labels,i
+
+def exp_full():
+	OX = [] 
+	labels = [] 
+	P = .9
+
+	N = 10
+
+	def one_iteration(P,OX,labels,N,a,b):
+		ISA = [] 
+		i = 0 
+		for j in range(N):
+			O,i, IS = S.simulate_basic(a,b)
+			ISA.append(np.array(IS))
+
+		mean = np.mean(ISA, axis=0).tolist()
+		OX.append(mean)
+		labels.append('β={} ω={}'.format(a,b))
+		return i 
+
+	one_iteration(P,OX,labels,N,.9,.1)
+	#P = round(P*.5,2)
+	i = one_iteration(P,OX,labels,N,.5,.5)
+	#P = round(P*.8,2)
+	i = one_iteration(P,OX,labels,N,.1,.9)
+
+	return OX,labels,i
+
+
+OX,labels,i = exp_stepwise()
+
+S.plot_multi_results(OX,i,labels)
--- simulator.py
+++ simulator.py
@@ -1,5 +1,10 @@
-import random,os,subprocess
+import random,os,subprocess,time,csv
 from math import ceil, floor 
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from sklearn.preprocessing import normalize
+import statistics as stats 
 
 class BARON:
 	def __init__(self):
@@ -13,6 +18,7 @@ class BARON:
 				resname: \"{}.res\";
 				TimName: \"{}.tim\";
 				OptName: \"{}.opt\";
+				MaxIter: 100; 
 				CplexLibName: \"{}\";}}""".format(fpath,fpath,fpath,cplex_lib_path)
 		return opt 
 
@@ -68,10 +74,9 @@ class BARON:
 
 	def run_subp(self,path):
 		bpath = os.environ.get('BARON_PATH')
-		cmd = [bpath, 'baron', path]
+		cmd = [bpath, path]
 		p = subprocess.Popen(cmd, stdout=subprocess.PIPE) 
 		p.wait()
-
 
 	def read_sol(self,index=0):
 		path = '{}/baron/{}.res'.format(os.environ['IOTDIV_PATH'],index)
@@ -88,10 +93,16 @@ class BARON:
 			line = line.split('			')
 			sol = eval(line[2])
 			v = line[0].lstrip()
-
 			V.append((v,sol))
 
-		return V 
+		objective_value = 0 
+		for line in f: 
+			if line.find('The above solution') > -1: 
+				s = line.split(':')
+				s = s[1].lstrip().rstrip()
+				objective_value = eval(s)
+		V.append(('OBJ',objective_value))
+		return V
 
 
 class BARONiotdiv(BARON):
@@ -103,7 +114,7 @@ class BARONiotdiv(BARON):
 		for i in range(m):
 			equations+='e{}: '.format(indexes[i])
 			for j in range(c):
-				equations+= 'x{}{}'.format(i,j)+'+'
+				equations+= 'x{}_{}'.format(i,j)+'+'
 			equations=equations[:-1]
 			equations+= '=={};\n'.format(limit) 
 		return equations 
@@ -114,7 +125,7 @@ class BARONiotdiv(BARON):
 		for i in range(c):
 			equations+='e{}: '.format(indexes[i])
 			for j in range(m):
-				equations+= 'x{}{}'.format(j,i)+'+'
+				equations+= 'x{}_{}'.format(j,i)+'+'
 			equations=equations[:-1]
 			equations+= '<={};\n'.format(limit) 
 		return equations 
@@ -124,21 +135,25 @@ class BARONiotdiv(BARON):
 		equation = 'e{}: '.format(index) 
 		for i in range(m):
 			for j in range(c): 
-				equation+= '{}*x{}{}+'.format(R[j],i,j)
+				equation+= '{}*x{}_{}+'.format(R[j],i,j)
 		equation=equation[:-1]
 		equation+= '-{}==0;\n'.format(var) 
 		return equation
 
 	def process_sol(self,X): 
 		V = self.read_sol()
+		obj = 0 
 		for v in V:
 			s = v[1]
 			v = v[0] 
 			if v[0] == 'x':
-				i = eval(v[1])
-				j = eval(v[2])  
+				S = v.split('_')
+				i = eval(S[0][1:])
+				j = eval(S[1])
 				X[i][j] = s 
-		return X 
+			if v.find('OBJ')>-1: 
+				obj = s 
+		return X, obj 
 
 	def gen_program(self,beta,omega,m,c,D,R,I):
 
@@ -153,7 +168,7 @@ class BARONiotdiv(BARON):
 		indexes = [] #an entry for each element of matrix X 
 		for i in range(m):
 			for j in range(c):
-				indexes.append(str(i)+str(j))
+				indexes.append(str(i)+'_'+str(j))
 		binary_vars   = self.binary_vars(indexes)
 		
 		#Equations: 
@@ -187,28 +202,139 @@ class Simulator:
 	def __init__(self):
 		pass 
 
-	def next_config(self,program,X):
+	def execute_baron(self,program,X):
 		BR = BARONiotdiv()
 		path = os.environ['IOTDIV_PATH']+'/baron/program.bar'
+		start = time.time()
 		BR.out_file(program,'0',path)
 		BR.run_subp(path)
-		print('OK') 
+		end = time.time()
+
+		#print('Solution produced in {} seconds.'.format(round(end-start,4))) 
+
 		return BR.process_sol(X)
 
+	def normalize(self,V): 
+		V = np.array(V).reshape(1,-1)
+		V = normalize(V)
+		V = V.tolist()[0]
+		return V  
 
-	def simulate_basic(self,beta,omega):
+
+	def update_R(self,X,R,c): 
+		'''
+		Examine each row, add one to the counter for the
+		configurations that were used in the selected row. 
+		'''
+		for x in X: 
+			R = [R[i] if x[i]==0 else R[i]+1 for i in range(c)]
+		return R #self.normalize(R)
+
+	def update_I(self,X,S,I,c): 
+		'''
+		Examine each attacker skill, add one to the counter for the
+		configurations that were used AND compromised. 
+		'''
+		for x in X: 
+			I = [I[i]+1 if S[i]==1 and x[i]==1 else I[i] for i in range(c)]
+		return I #self.normalize(I)
+
+	def count_compromised(self,X,S,I,c):
+		'''
+		Examine each attacker skill, add one to the compromised counter, 
+		if the skill matches the chosen configuration. 
+		'''
+		compromised = 0 
+
+		for x in X: 
+			c = sum([1 if S[i]==1 and x[i]==1 else 0 for i in range(c)]) 
+			if c > 0: 
+				compromised+=1 
+		return compromised
+
+	def update_attacker(self, sample_space, n, c):
+		chosen_indexes = random.sample(sample_space, n)
+		S = [1 if i in chosen_indexes else 0 for i in range(c)]
+		return S 
+
+	def plot_results(self,R, I, S, O, iterations):
+		fig, ax = plt.subplots()
+
+		x = [i for i in range(iterations)]
+		y = [item for item in O]
+		
+		line1, = ax.plot(x, y, label='Objective function value')
+		#line1.set_dashes([2, 2, 10, 2]) 
+		
+		#y = [item for item in I]
+		#line2, = ax.plot(x, y, label='Insecurity value')
+
+		ax.legend()
+		plt.savefig("fig.PDF",dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', format=None,
+        transparent=False, bbox_inches=None, pad_inches=0.1, metadata=None)
+
+	def plot_multi_results(self, OX, iterations, labels):
+		plt.rcParams['font.size'] = 16
+		plt.rcParams['font.weight'] = 'bold'
+
+		fig, ax = plt.subplots()
+		k = 1
+		markers = ['','*','o','+','-','x']
+		for O,LB in zip(OX,labels): 
+			x = [i for i in range(iterations)]
+			y = [item for item in O]
+			#f2 = interp1d(x, y, kind='cubic')
+			#M = max(x)
+			#xnew = np.linspace(0, M, endpoint=True)
+
+			line1, = ax.plot(x, y, label=LB)
+			#plt.plot(x, y, 'o', xnew, f2(xnew), '--')
+			k+=1 
+		
+		
+
+		ax.legend()
+		ax.set_xlabel('Iterations', fontsize=18)
+		ax.set_ylabel('Insecurity cost', fontsize=18)
+		ax.grid(color='.95',linestyle='-', linewidth=.5)
+		#ax.set_title('')
+		fig.tight_layout()
+		plt.savefig("fig.PDF",dpi=None, facecolor='w', edgecolor='w',
+		orientation='portrait', format=None,
+		transparent=False, bbox_inches=None, pad_inches=0.1, metadata=None)
+
+	def record_attackers(self, S):
+		f = open("attackers.csv","a+")
+		writer = csv.writer(f)
+		writer.writerow(S) 
+		f.close() 
+
+	def read_attackers(self): 
+		try: 
+			f = open("attackers.csv", "r")
+		except Exception as e: 
+			return None 
+		reader = csv.reader(f)
+		AS = [list(map(int,x)) for x in reader]
+		return AS 
+
+	def load_configs(self):
 		#Number of configurations
-		c = 8
+		c = 32
 		#Number of devices 
-		d = 4 
+		d = 8
 		#Machines per devices 
 		k = 1 
 		#Machines
 		m = d*k 
+		#Number of attackers 
+		nA = 4
+
 		#Configuration matrix
 		X = [[0 for i in range(c)] for i in range(m)] 
 		#Diversity parameter 
-		D = m // c 
+		D = c // m 
 		if D < 1: 
 			D = 1 
 		#Configuration repetition counter
@@ -217,15 +343,259 @@ class Simulator:
 		I = [0 for i in range(c)]
 		#Attacker capabilities 
 		p = .2 
-		chosen_indexes = random.sample([i for i in range(c)], ceil(c*p))
-		S = [1 if i in chosen_indexes else 0 for i in range(c)]
+		#Choose the space of samples for configurations.
+		#If each config appears once, then they all have the same
+		#probability of being vulnerable. We'll use a slightly
+		#different weight for some of them. 
+		sample_space = [i for i in range(c)]
+		#Repeat the first four configs twice each.
+		#That means, these first four are more vulnerable. 
+		for i in range(2):
+			sample_space += [i for i in range(0,4)]
 
-		#Start generating BARON program
+		return c,d,k,m,nA,X,D,R,I,p,sample_space
+
+	def normalized_mean(self, V): 
+		X = np.array([x for x in V])
+		X = normalize(X.reshape(1, -1))
+		m = np.mean(X)
+		print(m)
+		raise Exception('nothing')
+		return m 
+
+	def normalized_max(self, V): 
+		X = np.array([x for x in V])
+		X = normalize(X.reshape(1, -1)).tolist()
+		X = [sum(x) for x in X]
+		#print(X[0])
+		m = stats.mean(X)
+		#print(m)
+		#raise Exception('nothing')
+		return m 
+
+
+	def simulate_basic(self,beta,omega):
+		c,d,k,m,nA,X,D,R,I,p,sample_space = self.load_configs()
+
+		print('Simulating defense with beta {} and omega {}'.format(beta,omega))
+
+		n = ceil(c*p)
+
 		BR = BARONiotdiv()
 
-		program = BR.gen_program(beta,omega,m,c,D,R,I)
-		X = self.next_config(program,X)
+		#record of objective function values. 
+		O = []
+		iterations = 150
 
+		#record insecurity scores
+		IS = [] 
+
+		#Reset repetition rates every xr ticks. 
+		xr = ceil(iterations/ 100000) 
+
+		#If we have logged attacker, we'll use them. 
+		AS = self.read_attackers()
+		AS = [] 
+		if AS: 
+			l = len(AS)
+		else: 
+			l = 0  
+
+		for i in range(iterations):
+			#print("({})".format(i))
+			#Start generating BARON program
+			program = BR.gen_program(beta,omega,m,c,D,self.normalize(R),self.normalize(I))
+			X,obj = self.execute_baron(program,X)
+			O.append(obj) 
+
+			#print('Obj: {}\nConfig: {}'.format(obj,X))
+			
+			#Repeat for the number of attackers.
+			if l > 0:
+				S = AS[l-1]
+				l-=1  
+			else: 
+				S = self.update_attacker(sample_space, n, c)
+				self.record_attackers(S)
+
+			#print('Attacker skills:',S)
+			R = self.update_R(X,R,c)
+			I = self.update_I(X,S,I,c)
+
+			# print(R)
+			# print(I)
+			# print(S)
+			# print('X:')
+			# for x in X:
+			# 	print(x)
+			# print(obj)
+
+			IS.append(self.normalized_mean(I))
+			
+			# print('Repetition rates:',R)
+			# print('Insecurity rates:', I)
+			# print('Attacker, repetition, and insecurity rates updated.')
+
+			xr-=1 
+			if (xr == 0):
+				xr = ceil(iterations/ 10) 
+				R = [0 for i in range(c)]
+
+		return O,i+1, IS
+
+	def expand_attacker_skills(self, S): 
+		T = [] 
+		c = len(S)
+		for i in range(c): 
+			if S[i] == 1: 
+				T.append([0 if j!= i else 1 for j in range(c)])
+		return T 
+
+	'''
+	This type of the attacker is persistent and only reveals their skills 
+	gradually. That is, the attacker can compromise two types of the OS, 
+	they will not do so in one attack attempt. 
+	The attacker starts with one configuration and moves to another 
+	one at a time. This is expected to confuse the defense. 
+	'''
+	def simulate_stepwise(self,beta,omega): 
+		c,d,k,m,nA,X,D,R,I,p,sample_space = self.load_configs()
+
+		print('Simulating defense with beta {} and omega {}'.format(beta,omega))
+
+		n = ceil(c*p)
+
+		BR = BARONiotdiv()
+
+		#record of objective function values. 
+		O = []
+		iterations = 150
+
+		#record insecurity scores
+		IS = [] 
+
+		#Reset repetition rates every xr ticks. 
+		xr = ceil(iterations/ 10) 
+
+		#If we have logged attacker, we'll use them. 
+		AS = self.read_attackers()
+		if AS: 
+			l = len(AS)-1
+		else: 
+			l = -1  
+
+		ASX = [] 
+		for i in range(l):
+			T = self.expand_attacker_skills(AS[i])
+			#T*=4 
+			ASX += T 
+
+		for i in range(iterations):
+			#print("({})".format(i))
+			#Start generating BARON program
+			program = BR.gen_program(beta,omega,m,c,D,self.normalize(R),self.normalize(I))
+			X,obj = self.execute_baron(program,X)
+			O.append(obj) 
+
+			if len(ASX) == 0: 
+				break 
+
+			S = ASX.pop() 
+
+			R = self.update_R(X,R,c)
+			I = self.update_I(X,S,I,c)
+
+			# print(R)
+			# print(I)
+			# print(S)
+			# print('X:')
+			# for x in X:
+			# 	print(x)
+			# print(obj)
+
+			IS.append(self.normalized_mean(I))
+
+		return O, i+1, IS
+
+
+	'''
+	This simulator uses a defense that is not accurate in 
+	detecting attacks. That is, for some probability P, the
+	defense can detect if a specific configuration was compromised. 
+	'''
+
+	def simulate_inaccurate(self,beta,omega,P=.7):
+		c,d,k,m,nA,X,D,R,I,p,sample_space = self.load_configs()
+
+		recorded_I = I[:]
+
+		print('Simulating defense with probability {}'.format(P))
+		n = ceil(c*p)
+
+		BR = BARONiotdiv()
+
+		#record of objective function values. 
+		O = []
+		iterations = 150
+
+		#record insecurity scores
+		IS = [] 
+
+		#Reset repetition rates every xr ticks. 
+		xr = ceil(iterations/ 100000) 
+
+
+		#If we have logged attacker, we'll use them. 
+		AS = self.read_attackers()
+		AS = [] 
+		if AS: 
+			print('Loaded logged attacker.')
+			l = len(AS)
+		else: 
+			l = 0  
+
+		for i in range(iterations):
+			#print("({})".format(i))
+			#Start generating BARON program
+			program = BR.gen_program(beta,omega,m,c,D,self.normalize(R),self.normalize(I))
+			X,obj = self.execute_baron(program,X)
+			O.append(obj) 
+
+			#print('Obj: {}\nConfig: {}'.format(obj,X))
+			
+			#Repeat for the number of attackers.
+			if l > 0:
+				S = AS[l-1]
+				l-=1  
+			else: 
+				S = self.update_attacker(sample_space, n, c)
+				self.record_attackers(S)
+
+			#print('Attacker skills:',S)
+			#Update repetition score
+			R = self.update_R(X,R,c)
+
+			'''Update insecurity score given probability P.
+			We will keep two copies. One copy represents the true
+			insecurity that will be used to examine the results 
+			and one is used to compute the next configuration. '''
+
+			choices = random.choices([1,0], weights = [P,1-P], k=1)
+			choice = choices[0]
+			if (choice == 1):
+				I = self.update_I(X,S,I,c)
+			recorded_I = self.update_I(X,S,recorded_I,c)
+
+			IS.append(self.normalized_mean(recorded_I))
+		
+
+			#Reset repetition rate
+			# xr-=1 
+			# if (xr == 0):
+			# 	xr = ceil(iterations/ 10) 
+			# 	R = [0 for i in range(c)]
+
+		return O,i+1, IS
 		
 		
 		
